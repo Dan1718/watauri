c0f9273a07f235854da36e0905ebfa50fefc6318
Dan1718 <danielvincent1718@gmail.com>
2026-06-12 11:28:48 +0530

    Started wiring in the wa engine

---
+++ whatsapp-tauri/src-go/wa.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/base64"
+	"log"
+	"sync"
+
+	"github.com/skip2/go-qrcode"
+	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow/types/events"
+)
+
+type WAManager struct {
+	client *whatsmeow.Client
+	status string // "unauthenticated" | "connecting" | "connected"
+	qrCode string // base64 PNG data URL of the current QR
+	mu     sync.RWMutex
+}
+
+func newWAManager() (*WAManager, error) {
+
+	storeContainer, err := sqlstore.New("sqlite3", "file:wa-session.db?_foreign_keys=on", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	device, err := storeContainer.GetFirstDevice()
+	if err != nil {
+		return nil, err
+	}
+	client := whatsmeow.NewClient(device, nil)
+	wa := &WAManager{client: client}
+	if device.ID != nil {
+		wa.status = "connected"
+	}
+	client.AddEventHandler(func(evt interface{}) {
+		switch v := evt.(type) {
+		case *events.QR:
+			png, _ := qrcode.Encode(v.Code, qrcode.Medium, 256)
+			dataURL := "data:image/png;base64" + base64.StdEncoding.EncodeToString(png)
+			wa.mu.Lock()
+			wa.qrCode = dataURL
+			wa.status = "connecting"
+			wa.mu.Unlock()
+			log.Println("[wa] New QR Code generated")
+		case *events.Connected:
+			wa.mu.Lock()
+			wa.status = "connected"
+			wa.mu.Unlock()
+			log.Println("[wa] Connected to Whatsapp")
+		case *events.LoggedOut:
+			wa.mu.Lock()
+			wa.status = "unauthenticated"
+			wa.qrCode = ""
+			wa.mu.Unlock()
+			log.Println("[wa] logged out")
+		}
+	})
+
+	return wa, nil
+
+}
+func (wa *WAManager) StartPairing() error { return wa.client.Connect() }
+func (wa *WAManager) GetStatus() string {
+	wa.mu.RLock()
+	defer wa.mu.RUnlock()
+	return wa.status
+}
+func (wa *WAManager) GetQR() string {
+	wa.mu.RLock()
+	defer wa.mu.RUnlock()
+	return wa.qrCode
+}
+func (wa *WAManager) Disconnect() { wa.client.Disconnect() }

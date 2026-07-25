450a21b49b4f66d32c2b0dcd2447856c513e75ec
shouryadixitisverycool <shouryadixitisverycool@gmail.com>
2026-07-25 22:51:48 +0530

    style(chat): replace new chat icon

--- whatsapp-tauri/src/app/components/tab-panel/chats.tsx
+++ whatsapp-tauri/src/app/components/tab-panel/chats.tsx
@@ -1,5 +1,4 @@
 import {
-  ChatDotsIcon,
   DotsThreeVerticalIcon,
   MagnifyingGlassIcon,
   UsersThreeIcon,
@@ -204,7 +203,10 @@ export default function Chats({ selectedTab }: { selectedTab: string }) {
         </p>
         <section className="flex justify-between items-center gap-2">
           <TooltipWrapper showTooltip={false} onClick={openNewChatWindow}>
-            <ChatDotsIcon className="text-white size-6" weight="bold" />
+            <svg aria-hidden="true" className="size-6 text-white" fill="none" viewBox="0 0 24 24">
+              <path fill="currentColor" d="M9.53 13h1.98v1.97c0 .43.25.85.67.98a1 1 0 0 0 1.31-.94v-2.02h1.98c.43 0 .85-.25.98-.67a1 1 0 0 0-.94-1.31h-2.02V9.03c0-.43-.25-.85-.67-.98a1 1 0 0 0-1.31.94v2.02H9.49a1 1 0 0 0-.94 1.31c.13.42.55.67.98.67Z" />
+              <path fill="currentColor" fillRule="evenodd" d="M.94 5.53 3 8.85v8.48C3 18.81 4.2 20 5.67 20h13.66c1.48 0 2.67-1.2 2.67-2.67V6.67C22 5.19 20.8 4 19.33 4H1.8a1 1 0 0 0-.85 1.53ZM5 8.28v9.05c0 .37.3.67.67.67h13.66c.37 0 .67-.3.67-.67V6.67c0-.37-.3-.67-.67-.67H3.6L5 8.28Z" clipRule="evenodd" />
+            </svg>
           </TooltipWrapper>
           <TooltipWrapper showTooltip={false}>
             <DotsThreeVerticalIcon
